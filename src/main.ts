85105bc68be47dee85988b004a8883b63b0fe353
yehonatan604 <97249220+yehonatan604@users.noreply.github.com>
2023-05-28 01:02:18 +0300

    deleted comments

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { DomService } from "./helpers/services/dom.service";
 import { PokemonsListService } from "./helpers/services/pokemons-list.service";
 import { TableService } from "./helpers/services/table.service";
 
-let fetchService = new FetchService(); //1: take & print pokemons
-let domService = new DomService(fetchService); //2: make dom service
-let pokemonsListService = new PokemonsListService(fetchService); //3: fetch pokemons & map to list
-let tableService = new TableService(fetchService, domService); //4: render
+let fetchService = new FetchService(); 
+let domService = new DomService(fetchService); 
+let pokemonsListService = new PokemonsListService(fetchService); 
+let tableService = new TableService(fetchService, domService);
 
 await pokemonsListService.fillPokemonsList();
 let pokemons = pokemonsListService.getPokemonsList();
