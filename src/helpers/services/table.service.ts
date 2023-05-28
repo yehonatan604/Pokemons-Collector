90ebb713446f3bab47d8d9c945512305c320bc63
yehonatan604 <97249220+yehonatan604@users.noreply.github.com>
2023-05-28 04:34:18 +0300

    table header

--- src/helpers/services/table.service.ts
+++ src/helpers/services/table.service.ts
@@ -12,6 +12,7 @@ export class TableService {
 
   fillTable(pokemonsList: Pokemon[] | any) {
     this.pokemonsList = pokemonsList;
+    this.generateTableHeader();
     for (let item of pokemonsList) {
       let row = this.dom.pokemonsTable!.insertRow();
       for (const key in item) {
@@ -23,25 +24,47 @@ export class TableService {
     this.handleRows();
   }
 
+  generateTableHeader() {
+    var header = document.createElement("thead");
+    var headingRow = document.createElement("tr");
+
+    var headingCell1 = document.createElement("td");
+    var headingText1 = document.createTextNode("url");
+    headingCell1.appendChild(headingText1);
+    headingRow.appendChild(headingCell1);
+
+    var headingCell2 = document.createElement("td");
+    var headingText2 = document.createTextNode("name");
+    headingCell2.appendChild(headingText2);
+    headingRow.appendChild(headingCell2);
+    header.appendChild(headingRow);
+
+    this.dom.pokemonsTable!.appendChild(header);
+  }
+
   handleRows() {
     this.dom.getTableRows().forEach((row: HTMLTableRowElement, index: number) => {
-      row.addEventListener("click", async () => {
-        const pokemon: Pokemon = this.pokemonsList[index];
-        const response = await useFetch(pokemon.url, FetchTypes.json);  
-        this.dom.addButtonFunctionality(pokemon);
-        this.dom.fillSprites([
-          response.sprites.front_default,
-          response.sprites.back_default,
-          response.sprites.front_shiny,
-          response.sprites.back_shiny,
-        ]);
-        this.dom.fillDetails(new PokemonDetails(
-          capitalizeFirstLetter(pokemon.name),
-          reduceAbilities(response.abilities),
-          response.height,
-          response.base_experience
-        ));
+        if (index != 0) {
+          row.addEventListener("click", async () => {
+            const pokemon: Pokemon = this.pokemonsList[index-1];
+            const response = await useFetch(pokemon.url, FetchTypes.json);
+            this.dom.addButtonFunctionality(pokemon);
+            this.dom.fillSprites([
+              response.sprites.front_default,
+              response.sprites.back_default,
+              response.sprites.front_shiny,
+              response.sprites.back_shiny,
+            ]);
+            this.dom.fillDetails(
+              new PokemonDetails(
+                capitalizeFirstLetter(pokemon.name),
+                reduceAbilities(response.abilities),
+                response.height,
+                response.base_experience
+              )
+            );
+          });
+        }
       });
-    });
   }
 }
